6c9e49611d5e7ea9ba963f87860ea07623e6d052
lavanyaj3 <lavanya.j3@ibm.com>
2024-10-01 18:50:08 +0530

    Update extension.ts
    adding isDevMode as boolean 
    
    Signed-off-by: lavanyaj3 <lavanya.j3@ibm.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,16 @@
 import { commands, ExtensionContext, ExtensionMode } from "vscode";
 import { SetupGranitePage } from "./panels/setupGranitePage";
 import { Telemetry } from "./telemetry";
-
 export async function activate(context: ExtensionContext) {
   await Telemetry.initialize(context);
   const setupGraniteCmd = commands.registerCommand("vscode-granite.setup", async () => {
     await Telemetry.send("granite.commands.setup");
-    const isDevMode = context.extensionMode === ExtensionMode.Development;
+    const isDevMode: boolean = context.extensionMode === ExtensionMode.Development;
     SetupGranitePage.render(context.extensionUri, isDevMode);
   });
   context.subscriptions.push(setupGraniteCmd);
   const hasRunBefore = context.globalState.get('hasRunSetup', false);
-
-  if (!hasRunBefore || isDevMode) { 
+  if (!hasRunBefore) {
     await context.globalState.update('hasRunSetup', true);
     return commands.executeCommand('vscode-granite.setup');
   }
