996ceefed052aec45c72cd58c5ba14640e34126e
lavanyaj3 <lavanya.j3@ibm.com>
2024-10-01 18:57:07 +0530

    Update extension.ts
    for testing purpose comitting the earlier code
    
    Signed-off-by: lavanyaj3 <lavanya.j3@ibm.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,17 @@
-import { commands, ExtensionContext, ExtensionMode } from "vscode";
+import { commands, ExtensionContext } from "vscode";
 import { SetupGranitePage } from "./panels/setupGranitePage";
 import { Telemetry } from "./telemetry";
+
 export async function activate(context: ExtensionContext) {
   await Telemetry.initialize(context);
   const setupGraniteCmd = commands.registerCommand("vscode-granite.setup", async () => {
     await Telemetry.send("granite.commands.setup");
-    const isDevMode: boolean = context.extensionMode === ExtensionMode.Development;
-    SetupGranitePage.render(context.extensionUri, isDevMode);
+    SetupGranitePage.render(context.extensionUri, context.extensionMode);
   });
   context.subscriptions.push(setupGraniteCmd);
   const hasRunBefore = context.globalState.get('hasRunSetup', false);
-  if (!hasRunBefore) {
+  const isDevMode = process.env.OLLAMA_MOCK === 'true';
+  if (!hasRunBefore || isDevMode) {
     await context.globalState.update('hasRunSetup', true);
     return commands.executeCommand('vscode-granite.setup');
   }
