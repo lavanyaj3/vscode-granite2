8b609b58a638524ed6cbb4416dd74250e808964a
lavanyaj3 <lavanya.j3@ibm.com>
2024-10-01 18:32:27 +0530

    Update extension.ts
    updating extension.ts to remove the ollama-mock
    
    Signed-off-by: lavanyaj3 <lavanya.j3@ibm.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,19 @@
-import { commands, ExtensionContext } from "vscode";
+import { commands, ExtensionContext, ExtensionMode } from "vscode";
 import { SetupGranitePage } from "./panels/setupGranitePage";
 import { Telemetry } from "./telemetry";
 
 export async function activate(context: ExtensionContext) {
-  // Initialize telemetry
   await Telemetry.initialize(context);
-
-  // Register the setup command
   const setupGraniteCmd = commands.registerCommand("vscode-granite.setup", async () => {
     await Telemetry.send("granite.commands.setup");
-    SetupGranitePage.render(context.extensionUri, context.extensionMode);
+    const isDevMode = context.extensionMode === ExtensionMode.Development;
+    SetupGranitePage.render(context.extensionUri, isDevMode);
   });
   context.subscriptions.push(setupGraniteCmd);
-
-  // Check if the setup has already run using globalState
   const hasRunBefore = context.globalState.get('hasRunSetup', false);
 
-  // If the setup hasn't run before or if we are in dev mode, show the welcome page
-  if (!hasRunBefore || isDevMode) {
-    // Set the flag in global storage indicating that setup has been completed
+  if (!hasRunBefore) {
     await context.globalState.update('hasRunSetup', true);
-
-    // Execute the setup command to show the welcome page
     return commands.executeCommand('vscode-granite.setup');
   }
-
-  // No setup required if it has been run before and we're not in dev mode
 }
